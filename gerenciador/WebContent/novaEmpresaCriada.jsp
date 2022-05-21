89c422a408165a79fb44b59ae6dca3f86caefad9
Thiago Marçal <thiagomarcal1984@gmail.com>
2022-05-21 00:13:31 -0300

    Despachando a requisição.

--- gerenciador/WebContent/novaEmpresaCriada.jsp
+++ gerenciador/WebContent/novaEmpresaCriada.jsp
@@ -1,13 +1,13 @@
 <%
 	String titulo = "Introdução ao JSP";
-	String nomeEmpresa = "Alura";
-    System.out.println(nomeEmpresa); // Esta saÃ­da vai para o log do Tomcat.
+	String nomeEmpresa = (String)request.getAttribute("empresa");
+    System.out.println(nomeEmpresa); // Esta saída vai para o log do Tomcat.
 %>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="ISO-8859-1">
-<title>Página JSP</title>
+<title>P&aacute;gina JSP</title>
 </head>
 <body>
 	<h1><%= titulo %></h1>
--- gerenciador/src/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
+++ gerenciador/src/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -3,6 +3,8 @@ package br.com.alura.gerenciador.servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,9 +13,8 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(urlPatterns = "/novaEmpresa")
 public class NovaEmpresaServlet extends HttpServlet {
 	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("Cadastrando nova empresa.");
-		PrintWriter out = response.getWriter();
 		
 		String nomeEmpresa = request.getParameter("nome");
 		Empresa empresa = new Empresa();
@@ -22,6 +23,10 @@ public class NovaEmpresaServlet extends HttpServlet {
 		Banco banco = new Banco();
 		banco.adiciona(empresa);
 		
-		out.println("<html><body>Empresa " + empresa.getNome() + " cadastrada com sucesso!</body></html>");
+		// Chamar a página JSP:
+		RequestDispatcher rd = request.getRequestDispatcher("/novaEmpresaCriada.jsp");
+		
+		request.setAttribute("empresa", empresa.getNome());
+		rd.forward(request, response);
 	}
 }
