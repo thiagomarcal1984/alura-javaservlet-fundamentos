c69910045cca3e19521b2051e842198c368253ce
Thiago Marçal <thiagomarcal1984@gmail.com>
2022-05-20 21:59:19 -0300

    Enviando parâmetros.

---
+++ gerenciador/src/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -0,0 +1,21 @@
+package br.com.alura.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/novaEmpresa")
+public class NovaEmpresaServlet extends HttpServlet {
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		System.out.println("Cadastrando nova empresa.");
+		PrintWriter out = response.getWriter();
+		String empresa = request.getParameter("nome");
+		empresa = empresa == null ? "NÃO INFORMADA" : empresa;
+		out.println("<html><body>Empresa " + empresa + " cadastrada com sucesso!</body></html>");
+	}
+}
