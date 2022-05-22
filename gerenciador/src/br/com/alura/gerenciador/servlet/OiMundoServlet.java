8473dd4cbe9ce8ad2ea2b4bb89874c9610a7ca2b
Thiago Marçal <thiagomarcal1984@gmail.com>
2022-05-22 18:39:54 -0300

    Conhecendo o web.xml.

--- gerenciador/src/br/com/alura/gerenciador/servlet/OiMundoServlet.java
+++ gerenciador/src/br/com/alura/gerenciador/servlet/OiMundoServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = "/oi")
+//@WebServlet(urlPatterns = "/oi") // Remoção da anotação com o path /oi.
 public class OiMundoServlet extends HttpServlet{
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
